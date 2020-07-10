c5bcefe8a080e8f917be971f0e5293d53f0278e3
Kochetkov Danil <Kochetkov214@gmail.com>
2020-07-10 11:16:01 +0300

    Add first kata and tests

---
+++ Kata/1. Roman to arabic number/spec/romanToArabic.spec.js
@@ -0,0 +1,20 @@
+import romanToArabicNumberConvert from '../index';
+
+test('expect Roman number correctly converted to arabic', () => {
+    expect(romanToArabicNumberConvert('XVII')).toBe(17);
+    expect(romanToArabicNumberConvert('XXXV')).toBe(35);
+    expect(romanToArabicNumberConvert('CL')).toBe(150);
+})
+
+test('expect Roman number is valid', () => {
+    expect(romanToArabicNumberConvert('A')).toBe('Not valid number');
+    expect(romanToArabicNumberConvert('XIAIX')).toBe('Not valid number');
+    expect(romanToArabicNumberConvert('VIIIII')).toBe('Not valid number');
+    expect(romanToArabicNumberConvert('XXXVV')).toBe('Not valid number');
+    expect(romanToArabicNumberConvert('IIX')).toBe('Not valid number');
+})
+
+test('expect Roman number is correctly converted from lower case', () => {
+    expect(romanToArabicNumberConvert('xii')).toBe(12);
+    expect(romanToArabicNumberConvert('vcl')).toBe(145);
+})
